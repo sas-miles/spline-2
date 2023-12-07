7e93b75f016467a8bd67bb2e305a63eb458864c1
sas-miles <132398659+sas-miles@users.noreply.github.com>
2023-12-07 12:50:41 -0500

    resolved url and toggle

--- src/app.js
+++ src/app.js
@@ -6,15 +6,32 @@ let spline = null;
 let obj = null;
 let isToggled = false;
 
-// Initialize Spline only once
+function setInitialState() {
+  console.log('Setting initial state. Current URL:', window.location.pathname);
+  if (window.location.pathname === '/spline-03-02') {
+    if (!isToggled) {
+      console.log('Toggling state on');
+      isToggled = true;
+      spline.emitEvent('mouseDown', obj.name);
+    }
+  } else {
+    if (isToggled) {
+      console.log('Toggling state off');
+      isToggled = false;
+      spline.emitEventReverse('mouseDown', obj.name);
+    }
+  }
+}
+
 function initSpline() {
   if (spline) return; // Check if Spline is already initialized
 
   const canvas = document.getElementById('canvas3d');
   spline = new Application(canvas);
 
-  spline.load('https://prod.spline.design/14CHZTAPI5leC3Pi/scene.splinecode').then(() => {
+  spline.load('https://prod.spline.design/zBBaOmVltDuUag17/scene.splinecode').then(() => {
     obj = spline.findObjectById('1abf83a1-5d8c-4819-a952-7bcd8afbcb11');
+    setInitialState(); // Set initial state based on URL
   });
 }
 
@@ -68,14 +85,17 @@ barba.init({
       beforeEnter() {
         disableNavigation(); // Disable navigation at the start of the transition
       },
+      leave(data) {
+        // Fade out the current container
+        return gsap.to(data.current.container, {
+          opacity: 0,
+          duration: 0.5,
+        });
+      },
       enter(data) {
         const nextContainer = data.next.container;
         nextContainer.classList.add('fixed');
 
-        gsap.to(data.current.container, {
-          opacity: 0,
-          duration: 2,
-        });
         // Delay the start of the fade-in for the next container
         return gsap.to(nextContainer, {
           opacity: 1,
@@ -90,5 +110,5 @@ barba.init({
     },
   ],
 });
-
+console.log('Starting Spline initialization...');
 initSpline();
